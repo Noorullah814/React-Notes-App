030c3e299a9b41bccf0db36bca34b486e90043f6
Noorullah <itxnoor148@gmail.com>
2025-12-11 22:18:16 +0500

    Updated UI and styles

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,7 @@ const App = () => {
       <div className='flex flex-col lg:w-1/2 m-5'>
         <h1 className='text-center text-3xl font-bold my-5'>Add Notes</h1>
 
+      <div className='shadow-lg rounded-2xl p-6 border-1'>
         <form onSubmit={submitHandler}
           className='flex flex-col m-5'
         >
@@ -53,16 +54,18 @@ const App = () => {
           <button className='border-2 px-5 py-3 m-3 text-xl active:scale-95 bg-black text-white'
           >Add Notes</button>
         </form>
+        </div>
       </div>
-      <div className='flex flex-col gap-5 lg:border-l-2 lg:w-1/2 m-5'>
+      <div className='flex flex-col gap-5 lg:border-l-2 lg:w-1/2 m-5 p-6'>
         <h1 className='flex text-4xl justify-center font-bold'>Racent Notes</h1>
         <div className='flex flex-wrap h-full overflow-auto'>
           {task.map(function (elem, idx) {
-            return <div key={idx} className='w-70 h-80 bg-[url(https://media.istockphoto.com/id/185245773/photo/a-blank-black-noted-book-on-a-white-background.jpg?s=612x612&w=0&k=20&c=saLTbbLpwy6f11xF858ab1Uchrk4BJfUg9vPqhauRg0=)] bg-cover  p-10 flex flex-col justify-between '>
+            return <div key={idx} className='w-70 h-80 bg-[url(https://media.istockphoto.com/id/185245773/photo/a-blank-black-noted-book-on-a-white-background.jpg?s=612x612&w=0&k=20&c=saLTbbLpwy6f11xF858ab1Uchrk4BJfUg9vPqhauRg0=)] bg-cover  p-10 flex flex-col justify-between hover:scale-105 transition duration-300'>
               <div className='flex flex-col gap-3'>
                 <h1 className='leading-tight text-2xl font-bold text-white'>{elem.title}</h1>
                 <p className='leading-tight text-sm text-gray-400'>{elem.details}</p></div>
-              <button onClick={deleteNote}
+              <button onClick={() => deleteNote(idx)}
+
                 className='bg-red-700 w-full text-white font-bold rounded cursor-pointer active:scale-95'>Delete</button>
             </div>
 
